aa9ab9d38f9fda277739c18212c2e423da261f8a
webserg@gmail.com <webserg@gmail.com>
2014-06-07 14:54:40 +0300

    first version

---
+++ src/Percolation.java
@@ -0,0 +1,98 @@
+/**
+ *
+ */
+public class Percolation {
+    int[][] grid;
+    QuickFindUF uf;
+    final int N;
+    int top;
+    int bottom;
+
+    private static final int OPEN = 1;
+    private static final int CLOSE = 0;
+
+    /**
+     * create N-by-N grid, with all sites blocked
+     *
+     * @param n number
+     */
+    public Percolation(int n) {
+        N = n;
+        grid = new int[N][N];
+        uf = new QuickFindUF(N * N + 2);
+        top = N * N + 1;
+        bottom = N * N + 2;
+        for (int i = 0; i < N; i++) {
+            uf.union(top, seed(0, i));
+        }
+        for (int i = 0; i < N; i++) {
+            uf.union(bottom, seed(i, 0));
+        }
+    }
+
+    /**
+     * open site (row i, column j) if it is not already
+     *
+     * @param i number
+     * @param j number
+     */
+    public void open(int i, int j) {
+        idxCheck(i,j);
+        if (grid[i][j] == CLOSE) {
+            grid[i][j] = OPEN;
+            int cur = seed(i, j);
+            if (i - 1 >= 0 && isOpen(i - 1, j)) {
+                uf.union(cur, seed(i - 1, j));
+            }
+            if (i + 1 < N && isOpen(i + 1, j)) {
+                uf.union(cur, seed(i + 1, j));
+            }
+            if (j - 1 >= 0 && isOpen(i, j - 1)) {
+                uf.union(cur, seed(i, j - 1));
+            }
+            if (j + 1 < N && isOpen(i, j + 1)) {
+                uf.union(cur, seed(i, j + 1));
+            }
+        }
+    }
+
+    private int seed(int i, int j) {
+        idxCheck(i,j);
+        return N * i + j;
+    }
+
+    /**
+     * is site (row i, column j) open?
+     *
+     * @param i number
+     * @param j number
+     * @return boolean
+     */
+    public boolean isOpen(int i, int j) {
+        idxCheck(i,j);
+        return grid[i][j] == OPEN;
+    }
+
+    /**
+     * // is site (row i, column j) full?
+     *
+     * @param i row
+     * @param j column
+     * @return boolean
+     */
+    public boolean isFull(int i, int j) {
+        idxCheck(i,j);
+        return uf.connected(top, bottom) && uf.connected(top, seed(i, j));
+    }
+
+    /* Validate the indexes, throws an exception if not valid */
+    private void idxCheck(int i, int j){
+        if (i < 0 || i > N || j < 0 || j > N)
+            throw new IndexOutOfBoundsException("Position (" + i + ", " + j + ") out of bounds");
+    }
+
+    public boolean percolates(){
+        return uf.connected(top, bottom);
+    }
+}
+
---
+++ src/PercolationStats.java
@@ -0,0 +1,5 @@
+/**
+ * Created by webserg on 07.06.2014.
+ */
+public class PercolationStats {
+}
