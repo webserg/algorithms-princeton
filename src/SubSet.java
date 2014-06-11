72c7e1ad9fb9ece435217452e2f066a9d5a458c4
webserg@gmail.com <webserg@gmail.com>
2014-06-11 11:37:47 +0300

    working version

---
+++ src/SubSet.java
@@ -0,0 +1,14 @@
+public class SubSet {
+    public static void main(String[] args) {
+        final int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        while (StdIn.hasNextLine() && !StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            queue.enqueue(s);
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(queue.dequeue());
+        }
+    }
+}
