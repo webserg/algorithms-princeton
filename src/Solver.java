7cd0d3113b290b29684f73ae78d83ac7d2cc3ca7
webserg@gmail.com <webserg@gmail.com>
2014-07-15 14:41:53 +0300

    tests solver puzzle

--- src/Solver.java
+++ src/Solver.java
@@ -1,110 +1,163 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * Created on 10.07.2014.
  * solver of puzzle.
  */
 public class Solver {
-    private boolean isSolvable = false;
-    private boolean isTwinSolvable = false;
-    private List<Board> listSolution = new ArrayList<>();
+    private final Stack<Board> boards;
+    private int moves;
+    private boolean isSolvable;
 
-    private List<Board> listTwinSolution = new ArrayList<>();
+    private class SearchNode implements Comparable<SearchNode> {
+        private Board board;
+        private int moves;
+        private SearchNode previous;
+        private int cachedPriority = -1;
 
-    public Solver(Board initial) {
-        List<Board> prevBoard = new ArrayList<>();
-        List<Board> prevTwinBoard = new ArrayList<>();
-        listSolution.add(initial);
-        if (initial.isGoal() || initial.dimension() == 0) {
-            isSolvable = true;
-        } else {
-            Board board = initial;
-            Board twinBoard = initial.twin();
-            int i = 0;
-            while (!isSolvable && !isTwinSolvable) {
-                if (i > (initial.dimension() + 1) * 100) {
-                    break;
-                }
-                board = runAlgorithmStep(board, prevBoard, false);
-                twinBoard = runAlgorithmStep(twinBoard, prevTwinBoard, true);
-                i++;
+        SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+        }
+
+        private int priority() {
+            if (cachedPriority == -1) {
+                cachedPriority = moves + board.manhattan();
             }
+            return cachedPriority;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            if (this.priority() < that.priority()) {
+                return -1;
+            }
+            if (this.priority() > that.priority()) {
+                return 1;
+            }
+            return 0;
         }
     }
 
-    // find a solution to the initial board (using the A* algorithm)
-    private Board runAlgorithmStep(final Board board, List<Board> prevBoards, boolean isTwin) {
-        Iterator<Board> initItr = board.neighbors().iterator();
-        Board searchBoard = initItr.next();
-        while (initItr.hasNext() && prevBoards.contains(searchBoard)) {
-            searchBoard = initItr.next();
+    /*
+     * find a solution to the initial board (using the A* algorithm)
+     */
+    public Solver(Board initial) {
+        boards = new Stack<Board>();
+        if (initial.isGoal()) {
+            isSolvable = true;
+            this.boards.push(initial);
+            return;
         }
-        if (!isTwin) listSolution.add(searchBoard);
-        else listTwinSolution.add(searchBoard);
-        if (searchBoard.isGoal()) {
-            if (!isTwin) isSolvable = true;
-            else isTwinSolvable = true;
-            return searchBoard;
+        if (initial.twin().isGoal()) {
+            isSolvable = false;
+            return;
         }
-        prevBoards.clear();
-        prevBoards.add(board);
-        return searchBoard;
+
+        MinPQ<SearchNode> minPQ = new MinPQ<SearchNode>();
+        MinPQ<SearchNode> minPQTwin = new MinPQ<SearchNode>();
+        moves = 0;
+        Board board = initial;
+        Board boardTwin = initial.twin();
+        SearchNode node = new SearchNode(board, 0, null);
+        SearchNode nodeTwin = new SearchNode(boardTwin, 0, null);
+        minPQ.insert(node);
+        minPQTwin.insert(nodeTwin);
+        while (moves < 100) {
+            node = minPQ.delMin();
+            nodeTwin = minPQTwin.delMin();
+            board = node.board;
+            boardTwin = nodeTwin.board;
+            if (boardTwin.isGoal()) {
+                isSolvable = false;
+                return;
+            }
+            if (board.isGoal()) {
+                isSolvable = true;
+                this.boards.push(board);
+                while (node.previous != null) {
+                    node = node.previous;
+                    this.boards.push(node.board);
+                }
+                return;
+            }
+            node.moves++;
+            nodeTwin.moves++;
+            Iterable<Board> neighbors = board.neighbors();
+            for (Board neighbor : neighbors) {
+                if (node.previous != null
+                        && neighbor.equals(node.previous.board)) {
+                    continue;
+                }
+                SearchNode newNode = new SearchNode(neighbor, node.moves, node);
+                minPQ.insert(newNode);
+            }
+            Iterable<Board> neighborsTwin = boardTwin.neighbors();
+            for (Board neighbor : neighborsTwin) {
+                if (nodeTwin.previous != null
+                        && neighbor.equals(nodeTwin.previous.board)) {
+                    continue;
+                }
+                SearchNode newNode = new SearchNode(neighbor, nodeTwin.moves,
+                        nodeTwin);
+                minPQTwin.insert(newNode);
+            }
+            moves++;
+        }
     }
 
-
+    /*
+     * is the initial board solvable?
+     */
     public boolean isSolvable() {
         return isSolvable;
     }
 
+    /*
+     * min number of moves to solve initial board; -1 if no solution
+     */
     public int moves() {
         if (isSolvable) {
-//            for (Board b : listSolution) {
-//                System.out.println(b.toString());
-//            }
-            if (listSolution.size() == 0) return 0;
-            else return listSolution.size() - 1;
+            return boards.size() - 1;
         } else {
-//            if (listSolution != null && listSolution.size() > 1)
-//                System.out.println(listSolution.get(0).toString());
             return -1;
         }
-    }                      // min number of moves to solve initial board; -1 if no solution
-
-    public Iterable<Board> solution() {
-        if (!isSolvable) return null;
-        else return listSolution;
-    }      // sequence of boards in a shortest solution; null if no solution
-
-    public static void main(String[] args) {
-        runSolver(args[0]);
     }
 
-    private static int runSolver(String fileName) {
+    /*
+     * sequence of boards in a shortest solution; null if no solution
+     */
+    public Iterable<Board> solution() {
+        if (isSolvable) {
+            return boards;
+        } else {
+            return null;
+        }
+    }
+
+    /*
+     * solve a slider puzzle (given below)
+     */
+    public static void main(String[] args) {
         // create initial board from file
-        int res=-1;
-        In in = new In(fileName);
+        In in = new In(args[0]);
         int N = in.readInt();
         int[][] blocks = new int[N][N];
         for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++)
                 blocks[i][j] = in.readInt();
-            }
         Board initial = new Board(blocks);
 
-//        // solve the puzzle
+        // solve the puzzle
         Solver solver = new Solver(initial);
-//
-//        // print solution to standard output
+
+        // print solution to standard output
         if (!solver.isSolvable())
             StdOut.println("No solution possible");
         else {
-            res = solver.moves();
-            StdOut.println("Minimum number of moves = " + res);
+            StdOut.println("Minimum number of moves = " + solver.moves());
             for (Board board : solver.solution())
                 StdOut.println(board);
         }
-        return res;
     }
+
 }
--- src/TestSolver.java
+++ src/TestSolver.java
@@ -31,31 +31,90 @@ public class TestSolver {
         Assert.assertEquals(-1, solver.moves());
     }
 
+    private static int[][] getBlocks(String fileName) {
+        In in = new In(fileName);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        return blocks;
+    }
+
+
     @Test
     public void mainTest() {
-        Solver.main(new String[]{"puzzle04.txt"});
+        int[][] blocks = getBlocks("puzzle04.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(4, solver.moves());
     }
 
     @Test
     public void mainTestPuzzle01() {
-        Solver.main(new String[]{"puzzle01.txt"});
-
+        int[][] blocks = getBlocks("puzzle01.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(1, solver.moves());
     }
 
     @Test
     public void mainTestPuzzle02() {
-        Solver.main(new String[]{"puzzle02.txt"});
-
+        int[][] blocks = getBlocks("puzzle02.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(2, solver.moves());
     }
 
     @Test
     public void mainTestPuzzle03() {
-        Solver.main(new String[]{"puzzle03.txt"});
-
+        int[][] blocks = getBlocks("puzzle03.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(3, solver.moves());
     }
 
     @Test
     public void mainTestPuzzle10() {
-        Solver.main(new String[]{"puzzle10.txt"});
+        int[][] blocks = getBlocks("puzzle10.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(10, solver.moves());
+    }
+
+    @Test
+    public void mainTestPuzzle5() {
+        int[][] blocks = getBlocks("puzzle05.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(5, solver.moves());
+    }
+
+    @Test
+    @Ignore
+    public void mainTestPuzzle31() {
+        int[][] blocks = getBlocks("puzzle31.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(31, solver.moves());
+    }
+
+    @Test
+    @Ignore
+    public void mainTestPuzzle28() {
+        int[][] blocks = getBlocks("puzzle28.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(28, solver.moves());
+    }
+
+    @Test
+    @Ignore
+    public void mainTestPuzzle16() {
+        int[][] blocks = getBlocks("puzzle16.txt");
+        Board initial = new Board(blocks);
+        Solver solver = new Solver(initial);
+        Assert.assertEquals(16, solver.moves());
     }
 }
